a47d7069bc239060ad49266277cdd887955e5c1b
Divyam <divyam2003ds@gmail.com>
2025-04-18 11:39:48 +0530

    Allow all hosts in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,19 +2,17 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    allowedHosts: true, 
   },
-  plugins: [
-    react(),
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-      crypto: "crypto-browserify", 
+      crypto: "crypto-browserify",
     },
   },
 }));
